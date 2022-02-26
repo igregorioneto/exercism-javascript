a6cebe84d20aac5b9d954f5f4db53abb1e1265d7
Gregório Neto <igregoriodoneto@outlook.com>
2022-02-25 21:44:12 -0300

    Usando array para identificar senha do poema

---
+++ poetry-club-door-policy/index.js
@@ -0,0 +1,9 @@
+const CODE_WORK = [
+    'Compilers intensily bestow',
+    'On commencing without ego',
+    'Different processes ajar',
+    'Exit with zero quick',
+  ];
+  const value = CODE_WORK[0].trim();
+
+  console.log(value[value.length - 1]);
